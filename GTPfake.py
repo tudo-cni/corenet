49230c655d1ab513909746b4937678c046214733
Fabian Eckermann <fabian.eckermann@googlemail.com>
2018-02-28 16:02:36 +0100

    Optimized for tinyLTE

--- GTPfake.py
+++ GTPfake.py
@@ -3,7 +3,10 @@
 # GTPU handler library to work with corenet.py & libmich to use the kernel-mode gtpu implementation
 
 import os
+import subprocess
 from libmich.mobnet.utils import log
+
+import tun_conf as config
 
 class GTPfake(object):
 
@@ -11,22 +14,37 @@ class GTPfake(object):
     MOD = []
 
     INT_IP = '192.168.4.210'
+    
+    UE_SUBNET_PREFIX = None
 
     GTPif = "gtp1"
-    CMDprefix = "ip netns exec gtpu " # run commands in network namespace 'gtpu'
     #CMDgtp_tunnel = "gtp-tunnel"
     CMDgtp_tunnel = "/usr/local/bin/gtp_tunnel_mgmt.sh"
 
     ip2teid = dict()
 
+    def __init__(self):
+        self.start() # like AuC
+
     def start(self):
-        self._log('INF', "GTPU kernel gtpu started")
-        self._log('INF', "GTP interface: %s" % self.GTPif)
-        self._log('INF', "CMD prefix: %s" % self.CMDprefix)
+        self._log('INF', "GTPU kernel gtpu started, GTP interface: {0}".format(self.GTPif))
+
         # TODO: check if interface is available, if not create & configure
-        #   gtp-link add gtp1
-        #   ip link set gtp1 mtu 1500
-        #   ip r a 192.168.3.0/24 dev gtp1
+        gtp_link = subprocess.Popen(['gtp-link', 'add', self.GTPif], stderr = subprocess.PIPE)
+        while True:
+            if ('WARNING: attaching dummy socket descriptors. Keep '
+			    'this process running for testing purposes.') in gtp_link.stderr.readline():
+                break
+        self._log('INF', 'gtp link added')
+        subprocess.call(['ip', 'link', 'set', self.GTPif, 'mtu', '1500'])
+        subprocess.call(['ip', 'route', 'add', self.UE_SUBNET_PREFIX + '.0/24', 'dev', self.GTPif])
+        # Dummy IP goes back to host (tunneling)
+        my_virt_ip = config.OWN_VIRT_IP
+        ip_temp = my_virt_ip.split('.')
+        ip_temp[2] = '100'
+        own_ip = '.'.join(ip_temp)
+        
+        subprocess.call(['ip', 'route', 'add', own_ip, 'dev', 'eth0'])
 
     def stop(self):
         # TODO: delete interface
@@ -38,24 +56,29 @@ class GTPfake(object):
         if logtype in self.DEBUG:
             log('[{0}] [GTPfake] {1}'.format(logtype, msg))
 
-    def add_mobile(self, mobile_IP='192.168.3.100', rnc_IP='192.168.4.90', TEID_from_rnc=0x1, TEID_to_rnc=0x1):
+    def add_mobile(self, mobile_IP='192.168.1.201', rnc_IP='10.1.1.1', TEID_from_rnc=0x1, TEID_to_rnc=0x1):
         # gtp-tunnel add <gtp device> <v1> <i_tei> <o_tei> <ms-addr> <sgsn-addr>
         #self._log(msg="add_mobile(%s,%s,%d,%d)" % (mobile_IP, rnc_IP,TEID_from_rnc, TEID_to_rnc))
         if self.ip2teid.has_key(mobile_IP):
             self.rem_mobile(mobile_IP)
-        cmd = "%s %s add %s v1 %d %d %s %s" % (self.CMDprefix, self.CMDgtp_tunnel, self.GTPif, TEID_from_rnc, TEID_to_rnc, mobile_IP, rnc_IP)
-        self._log(msg="running cmd: %s"%cmd)
-        os.system(cmd)
+        cmd = [self.CMDgtp_tunnel, 'add', self.GTPif, 'v1', str(TEID_from_rnc),
+               str(TEID_to_rnc), mobile_IP, rnc_IP]
+        #self._log('INF', "running cmd: " + str(cmd))
+        subprocess.call(cmd)
         self.ip2teid[mobile_IP] = TEID_from_rnc
+        self._log('INF', 'setting GTP tunnel for mobile with IP {0}'.format(
+                  mobile_IP))
 
-    def rem_mobile(self, mobile_IP='192.168.3.100'):
+    def rem_mobile(self, mobile_IP='192.168.1.201'):
         #gtp-tunnel del <gtp device> <version> <tid>
         #self._log(msg="rem_mobile(%s)"%mobile_IP)
         teid = self.ip2teid.get(mobile_IP, None)
         if not teid is None:
-            cmd = "%s %s del %s v1 %d" % (self.CMDprefix, self.CMDgtp_tunnel, self.GTPif, teid)
-            self._log(msg="running cmd: %s"%cmd)
-            os.system(cmd)
+            cmd = [self.CMDgtp_tunnel, 'del', self.GTPif, 'v1', str(teid)]
+            #self._log('INF', "running cmd: " + str(cmd))
+            subprocess.call(cmd)
             self.ip2teid.pop(mobile_IP)
+            self._log('INF', 'unsetting GTP tunnel for mobile with IP '\
+                      '{0}'.format(mobile_IP))
         else:
             self._log('WNG', "cant find TEID for ip %s" % mobile_IP)
--- corenet.py
+++ corenet.py
@@ -27,8 +27,24 @@
 # *--------------------------------------------------------
 #*/
 
+# Optimized for CNI by Caner Bektas (caner.bektas@tu-dortmund.de)
+# Prepared for Docker Integration and further optimized for tinyLTE by
+# Philipp Gorczak and Fabian Eckermann {philipp.gorczak, fabian.eckermann}@tu-dortmund.de
+
+# For extracting interface information
+import netifaces as ni
+
 # IPython interactive shell
+import re
+import signal
+import subprocess
 import sys
+
+import corenet_conf as config
+
+# Import all Libs in following folder (for CryptoMobile)
+sys.path.append("/usr/corenet/deps")
+
 from time import sleep
 from IPython.terminal.embed import InteractiveShellEmbed
 #
@@ -36,66 +52,93 @@ from libmich.utils.repr import *
 from libmich.formats.L3Mobile import parse_L3
 from libmich.mobnet.MME import *
 from libmich.mobnet.AuC import AuC
-#from libmich.mobnet.GTPmgr import *
-from libmich.mobnet.GTPmgr import ARPd
+from libmich.mobnet.ARPmgr import ARPd
 from libmich.mobnet.SMSmgr import SMSRelay
 from libmich.mobnet.utils import mac_aton
 from GTPfake import GTPfake as GTPUd
 
-# list of UE IMSI and associated IP address and phone number (for SMS)
-# those IMSI needs to be configured in libmich/mobnet/AuC.db too
-UE = {'001010000000001': {'IP': '192.168.3.201', 'Num': '0001'},
-      '001010000000002': {'IP': '192.168.3.202', 'Num': '0002'},
-      '001012342001266': {'IP': '192.168.3.202', 'Num': '0003'},
-      '001022342000102': {'IP': '192.168.3.203', 'Num': '0004'},
-      '208930000000002': {'IP': '192.168.3.204', 'Num': '0005'},
-      }
+### Initial settings ###
+#SGi (External Net) and S1 (eNodeB) Interface names
+sgi_if = 'eth0'
+s1_if = 'eth1'
+
+# SCTP and GTP Port
+sctp_port = 36412
+gtp_port = 2152
+
+# MAC- and IP-address of gateway to external network
+#(SGi Interface is in LAN with SDN-Network, so define self as Gateway)
+def get_gateway_mac(gateway_ip):
+    """ HACK for getting the gateway MAC using its IP. """
+    subprocess.check_output(['ping', '-c', '1', gateway_ip])
+    arp = subprocess.check_output(['arp', '-n']).splitlines()
+    gw_mac_re = re.compile(r'^.*ether\s+([0-9a-f:]{17})')
+    for l in arp:
+        if l.startswith(gateway_ip):
+            return gw_mac_re.match(l).group(1)
+    else:
+        raise RuntimeError('Could not resolve gateway MAC.')
+
+# IP given to UE on SGi (External)
+ue_subnet_prefix = config.UE.itervalues().next()['IP'][:-4]
+ue_ip_pool = []
 
 def main():
+    UE = config.UE
+
+    gw_mac = get_gateway_mac(config.SGI_GW_IP)
+
+    # Get IP and MAC address of the Intefaces
+    ip = ni.ifaddresses(sgi_if)[2][0]['addr']
+    mac = ni.ifaddresses(sgi_if)[17][0]['addr']
+    sgi_ip = str(ip)
+    sgi_mac = str(mac)
+
+    ip = ni.ifaddresses(s1_if)[2][0]['addr']
+    s1_ip = str(ip)
 
     # debugging / tracing level
-    AuC.DEBUG = ('ERR', 'WNG', 'INF', 'DBG')
+    AuC.DEBUG = ('ERR', 'WNG', 'INF')
+    AuC.AuC_db_path = config.AUC_DB_PATH
     ARPd.DEBUG = ('ERR', 'WNG', 'INF')
-    GTPUd.DEBUG = ('ERR', 'WNG', 'INF', 'DBG')
-    SMSRelay.DEBUG = ('ERR', 'WNG', 'INF', 'DBG')
-    MMEd.DEBUG = ('ERR', 'WNG', 'INF', 'DBG')
+    GTPUd.DEBUG = ('ERR', 'WNG', 'INF')#, 'DBG')
+    SMSRelay.DEBUG = ('ERR', 'WNG', 'INF')#, 'DBG')
+    MMEd.DEBUG = ('ERR', 'WNG', 'INF')#, 'DBG')
     MMEd.TRACE_SK = False
     MMEd.TRACE_ASN1 = False
     MMEd.TRACE_SEC = False
     MMEd.TRACE_NAS = False
     MMEd.TRACE_SMS = False
-    MMEd_server_addr = ('192.168.4.80', 36412)
-
-    ### AuC config ###
-    # authentication center
-    AuC.OP = 'ffffffffffffffff'
+    MMEd_server_addr = (s1_ip, sctp_port)
 
     ### ARPd config ###
     # ARP resolver for the PGW on SGi
-    ARPd.GGSN_ETH_IF = 'br-neta' # ethernet interface toward external networks (e.g. Internet)
-    ARPd.GGSN_MAC_ADDR = '1e:52:06:07:1c:4b' # MAC address of the ethernet interface toward external networks
-    ARPd.GGSN_IP_ADDR = '192.168.1.80' # IP address set to the ethernet interface toward external networks
+    ARPd.GGSN_ETH_IF = sgi_if
+    ARPd.GGSN_MAC_ADDR = sgi_mac
+    ARPd.GGSN_IP_ADDR = sgi_ip
     # IP given to UE on SGi
-    ARPd.SUBNET_PREFIX = '192.168.1' # subnet prefix of the LAN to which the ethernet interface to external network is connected
-    ARPd.IP_POOL = []
+    ARPd.SUBNET_PREFIX = ue_subnet_prefix
+    ARPd.IP_POOL = ue_ip_pool
     for ue_config in UE.values():
         ARPd.IP_POOL.append(ue_config['IP'])
     # IPv4 1st hop after PGW on SGi
-    ARPd.ROUTER_MAC_ADDR = 'ae:85:d8:35:2f:2b' # the LAN router (1st IP hop) MAC address
-    ARPd.ROUTER_IP_ADDR = '192.168.1.43' # the LAN router (1st IP hop) IP address
+    ARPd.ROUTER_MAC_ADDR = gw_mac
+    # ARPd.ROUTER_IP_ADDR = gw_ip
 
     ### GTPUd config ###
     # GTP user-plane packets forwarder
     # internal interface (SGW S1U user-plane)
-    GTPUd.INT_IP = '192.168.4.210' # IP address exposed on the RAN side
-    GTPUd.INT_PORT = 2152
+    GTPUd.INT_IP = s1_ip
+    GTPUd.INT_PORT = gtp_port
+    GTPUd.UE_SUBNET_PREFIX = ue_subnet_prefix
+    
     # GTP User-Plane handler external interface (PGW SGi)
     GTPUd.EXT_IF = ARPd.GGSN_ETH_IF # same as ARPd.GGSN_ETH_IF
     GTPUd.GGSN_MAC_ADDR = ARPd.GGSN_MAC_ADDR # same as ARPd.GGSN_MAC_ADDR
     # mobile traffic filtering
-    #GTPUd.BLACKHOLING = False # allow all the traffic from handsets
+    GTPUd.BLACKHOLING = False # allow all the traffic from handsets
     #GTPUd.BLACKHOLING = 'ext' # allow only the traffic on the local LAN, and on allowed ports for external networks
-    GTPUd.BLACKHOLING = True # discard all the traffic from handsets, except on allowed ports
+    #GTPUd.BLACKHOLING = True # discard all the traffic from handsets, except on allowed ports
     GTPUd.WL_ACTIVE = True # enables whitelist ports when BLACKHOLING is enabled
     GTPUd.WL_PORTS = [('UDP', 53), ('UDP', 123)] # allow only those ports when BLACKHOLING is enabled
 
@@ -105,13 +148,13 @@ def main():
 
     ### MME config ###
     MMEd.ConfigS1 = {
-    'MMEname': 'CorenetMME', # optional
+    'MMEname': config.MME_NAME, # optional
     'ServedGUMMEIs': [
-        {'servedPLMNs': [bytes(PLMN('00101'))],
-         'servedGroupIDs': ['\x00\x01'],
-         'servedMMECs': ['\x01']}
+        {'servedPLMNs': [bytes(PLMN(config.MCC + config.MNC))], #(PLMN = MCC + MNC - First 5 digits of IMSI)
+         'servedGroupIDs': [unhexlify(config.MME_GROUP)],
+         'servedMMECs': [unhexlify(config.MME_CODE)]}
         ], # mandatory, at least 1
-    'RelativeMMECapacity': 20, # mandatory, 0 < X < 255
+    'RelativeMMECapacity': 100, # mandatory, 0 < X < 255
     'MMERelaySupportIndicator': None, # optional
     'CriticalDiagnostics': None, # optional, 'true' otherwise
     }
@@ -122,48 +165,34 @@ def main():
     UEd.TRACE_SMS = True # trace all SMS procedures
     # NAS security
     UEd.NASSEC_MAC = True # enforce NAS security
-    UEd.NASSEC_ULCNT = False # enforce NAS UL count
+    # UEd.NASSEC_ULCNT = True # enforce NAS UL count
     UEd.AUTH_POL_ATT = 1 # attach authentication policy
-    UEd.AUTH_POL_TAU = 0 # TAU authentication policy
+    UEd.AUTH_POL_TAU = 1 # TAU authentication policy
     UEd.AUTH_POL_SERV = 10 # service request authentication policy
-    UEd.AUTH_AMF = b'\x80\x00' # Authentication Management Field
     UEd.SMC_IMEI_POL = 1 # request IMEISV only once during security mode ctrl
-    UEd.SMC_EEA = [0, 1, 2, 3] # encryption algorithm priority: 0:None, 1:SNOW, 2:AES, 3:ZUC
-    UEd.SMC_EIA = [1, 2, 3] # integrity protection algorithm priority: 0:None (emergency call only), 1:SNOW, 2:AES, 3:ZUC
+    #UEd.SMC_EEA = [0, 1, 2, 3] # encryption algorithm priority: 0:None, 1:SNOW, 2:AES, 3:ZUC
+    #UEd.SMC_EIA = [1, 2, 3] # integrity protection algorithm priority: 0:None (emergency call only), 1:SNOW, 2:AES, 3:ZUC
+    UEd.SMC_EEA = [0] # encryption algorithm priority: 0:None, 1:SNOW, 2:AES, 3:ZUC
+    UEd.SMC_EIA = [1, 2] # integrity protection algorithm priority: 0:None (emergency call only), 1:SNOW, 2:AES, 3:ZUC
     # ATTACH extended features
     UEd.ATT_EQU_PLMN = None # equivalent PLMNs
     UEd.ATT_ECN_LIST = None # list of (emergency service category, emergency number)
     UEd.ATT_EPS_FEAT = None # EPS network feature support
     UEd.ATT_ADD_UPD = None
     # APN
-    UEd.ESM_APN_DEF = 'default' # default APN
-    UEd.ESM_PDN = {
-        'corenet': {
-            'IP':[1, '0.0.0.0'], # PDN type (1:IPv4, 2:IPv6, 3:IPv4v6), UE IP@ will be set at runtime
-            'DNS':['192.168.1.1', '8.8.8.8'], # 2 IP@ for DNS servers
-            'QCI': 9, # QoS class id (9: internet browsing), NAS + S1 parameter
-            'PriorityLevel': 15, # no priority (S1 parameter)
-            'PreemptCap': 'shall-not-trigger-pre-emption', # or 'may-trigger-pre-emption' (S1 parameter)
-            'PreemptVuln': 'not-pre-emptable', # 'pre-emptable' (S1 parameter)
-            },
-            'default': {
-                'IP':[1, '0.0.0.0'], # PDN type (1:IPv4, 2:IPv6, 3:IPv4v6), UE IP@ will be set at runtime
-                'DNS':['192.168.1.40', '8.8.8.8'], # 2 IP@ for DNS servers
-                'QCI': 9, # QoS class id (9: internet browsing), NAS + S1 parameter
-                'PriorityLevel': 15, # no priority (S1 parameter)
-                'PreemptCap': 'shall-not-trigger-pre-emption', # or 'may-trigger-pre-emption' (S1 parameter)ipshell = InteractiveShellEmbed(
-                'PreemptVuln': 'not-pre-emptable', # 'pre-emptable' (S1 parameter)
-                },
-
-        } # list of configured APN
-
+    UEd.ESM_APN_DEF = config.DEF_APN # default APN
+    UEd.ESM_PDN = config.PDN
 
     # start AuC, ARPd and GTPUd, MMEd
     log('\n\n\n', withdate=False)
     log('--------########<<<<<<<<////////:::::::: CORENET ::::::::\\\\\\\\\\\\\\\\>>>>>>>>########--------')
     log('...')
     AUCd = AuC()
-    GTPd = GTPUd() # this starts ARPd automatically
+    UEd.AUTH_AMF = AUCd.AMF # Authentication Management Field
+    # and finally start ARP resolver
+    ARP = ARPd()
+    GTPUd.ARPd = ARP
+    GTPd = GTPUd() # this starts ARPd automatically !!! not if gtpfake
     SMSd = SMSRelay()
     MME = MMEd(config={'server': MMEd_server_addr,
                        'ue': UE,
@@ -185,6 +214,7 @@ def main():
 
     def stop():
         AUCd.stop()
+        ARPd.stop()
         GTPd.stop()
         SMSd.stop()
         MME.stop()
@@ -228,11 +258,18 @@ def main():
     _ipy_ns['EMMInformation'] = EMMInformation
     _ipy_ns['MMEDetach'] = MMEDetach
 
-    # interactive epc session
-    ipshell = InteractiveShellEmbed(user_ns=_ipy_ns, \
-                                    banner1=_ipy_banner, \
-                                    exit_msg='leaving corenet now...')
-    ipshell()
+    # TODO use click
+    if len(sys.argv) >= 2 and sys.argv[1] == '--interactive':
+        # interactive epc session
+        ipshell = InteractiveShellEmbed(user_ns=_ipy_ns, \
+                                        banner1=_ipy_banner, \
+                                        exit_msg='leaving corenet now...')
+        ipshell()
+    else:
+        try:
+            signal.pause()
+        except KeyboardInterrupt:
+            pass
     #
     # before exiting, we need to close everything properly
     # MMEd, GTPUd, AuC, SMSRelay ...
